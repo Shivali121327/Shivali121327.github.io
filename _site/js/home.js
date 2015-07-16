32660c27396f4500a6120c06285b1feeab3387e2
shivali <chansoriyashivali@gmail.com>
2015-07-16 17:51:16 +0530

    Avoided overlaping of elements on resizing

--- _site/js/home.js
+++ _site/js/home.js
@@ -9,15 +9,15 @@ $(document).ready(function() {
 	  // callback
 	  complete: $.noop
 	});
-	$(function(){
-    $(".nav a").click(function(e){
-      e.preventDefault();
-      $('html,body').scrollTo(this.hash,this.hash); 
-    });
-  });
 	$('body').scrollspy({ target: '#navbar-example' })
 	$('[data-spy="scroll"]').each(function () {
   var $spy = $(this).scrollspy('refresh')
   })
+  $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+  });
 });
 new WOW().init();
